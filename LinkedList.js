2e1c7c29988eb96c6ed9c7c49e094b88d27e9eaf
Shing Wong <shing2487@gmail.com>
2018-10-12 17:52:10 -0400

    initial implementation of linked list

---
+++ LinkedList.js
@@ -0,0 +1,20 @@
+// Linked List is a linear collection of data elements, order is not given by their physical placement in memory.
+// every Linked List Node has a null
+// null by itself is a valid Linked List
+// the first node is called the HEAD
+// Linked List basic operations: Insert, Search, Delete
+
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = head;
+    this.tail = tail;
+    this.length = length;
+  }
+}
