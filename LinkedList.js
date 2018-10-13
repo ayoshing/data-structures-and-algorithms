c44b4527629e94dad316fd3d201adcf5760f6161
Shing Wong <shing2487@gmail.com>
2018-10-12 21:02:23 -0400

    updated linked list

--- LinkedList.js
+++ LinkedList.js
@@ -1,20 +1,66 @@
-// Linked List is a linear collection of data elements, order is not given by their physical placement in memory.
-// every Linked List Node has a null
-// null by itself is a valid Linked List
-// the first node is called the HEAD
-// Linked List basic operations: Insert, Search, Delete
+// linked list is a linear collection of data elements, order is not given by their physical placement in memory.
+// every linked list Node points to a null
+// null by itself is a valid linked list
 
-class LinkedListNode {
+class Node {
   constructor(value, next = null) {
     this.value = value;
-    this.next = next;
+    this.next = null;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.head = head;
-    this.tail = tail;
-    this.length = length;
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    let node = new Node(value);
+    if (!this.head && !this.tail) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = this.tail.next;
+    }
+    this.length++;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  contains(value) {
+    let node = this.head;
+    while (node) {
+      if (node.value === value) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  }
+
+  remove(value) {
+    let node = this.head;
+    let previousNode;
+    while (node) {
+      if (node.value === value) {
+        previousNode = this.head;
+        this.head = node.next;
+      } else if (node.next.value === value) {
+        previousNode = node;
+        node = node.next;
+        break;
+      } else {
+        return false;
+      }
+    }
+    previousNode.next = node.next;
+    let temp = node.value;
+    node = null;
+    return temp;
   }
 }
